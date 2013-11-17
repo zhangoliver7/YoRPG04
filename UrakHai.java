152764fb875f7b25087435d566228cca49cb52ae
nperrotti <nicholas.perrotti79@gmail.com>
2013-11-17 18:45:12 -0500

    Update UrakHai.java

--- UrakHai.java
+++ UrakHai.java
@@ -1,3 +1,38 @@
-public class UrakHai extends Character{
+public class Monster extends Character {
+
+
+        public Monster() {
+                int random = (int)(Math.random() * 45);
+                random += 20;
+                strength = random + 15;
+                hp = 150;        
+                defense = 10;        
+                attRating = 1.0;
+        }
+
+        public String toString() {
+                String retStr = "Monster " + hp + " " + strength + " " + defense + " " + attRating;
+                return retStr;
+        }
+
+        
+
+        public void normalize()
+        {};
+        
+        public void specialize()
+        {};
+
+        public void specialAtt() {
+                hp -= 20;
+                int damage =(int)(strength * attRating - tom.getDefense());
+                tom.lowerHP(damage * 3);
+                return damage;
+        
+        }
+        
+}
+
+   
 
 }
