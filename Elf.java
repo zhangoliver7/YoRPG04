70041a8283306876894f161dcdf6d4958fae4ab1
nperrotti <nicholas.perrotti79@gmail.com>
2013-11-17 17:25:07 -0500

    Update Elf.java

--- Elf.java
+++ Elf.java
@@ -1,3 +1,36 @@
 public class Elf extends Character{
+    private String name;
+
+    public Warrior(String s) {
+        name = s;
+        hp = 125;
+        strength = 110;
+        defense = 15;
+        attRating = 0.4;
+    }
+    
+    public String toString() {
+        String retStr = name + " " + hp + " " + strength + " " + defense + " " + attRating;
+        return retStr;
+    }
+    
+    public String getName() {
+        return name;
+    }
+   
+
+    public void specialize() {
+        defense -= 5;
+        attRating += 0.2;
+    }        
+
+    public void normalize() {
+        defense = 20;
+        attRating = 0.4;
+    }        
+
+    public void specialAtt() {
+        
+    }
 
 }
