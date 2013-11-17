bd5893dc9e3e45c3f22a56281732052e1cc4321e
nperrotti <nicholas.perrotti79@gmail.com>
2013-11-17 18:03:51 -0500

    Update Wizard.java

--- Wizard.java
+++ Wizard.java
@@ -1,3 +1,40 @@
 public class Wizard extends Character{
+    private String name;
+
+    public Wizard(String s) {
+        name = s;
+        hp = 125;
+        strength = 120;
+        defense = 5;
+        attRating = 0.4;
+    }
+    
+    public String toString() {
+        String retStr = name + " " + hp + " " + strength + " " + defense + " " + attRating;
+        return retStr;
+    }
+    
+    public String getName() {
+        return name;
+    }
+   
+
+    public void specialize() {
+        defense -= 5;
+        attRating += 0.2;
+    }        
+
+    public void normalize() {
+        defense = 20;
+        attRating = 0.4;
+    }        
+
+    public void specialAtt() {
+        hp -= 25;
+        int damage =(int)(strength * attRating - tom.getDefense());
+        tom.lowerHP(damage * 4);
+        return damage;
+        
+    }
 
 }
