a24093c81270a92c9912ec2bc64bf5e90ded5d79
zhangoliver7 <zhangoliver7@gmail.com>
2013-11-17 19:31:15 -0500

    Update YoRPG.java

--- YoRPG.java
+++ YoRPG.java
@@ -15,8 +15,9 @@ public class YoRPG {
     public final static int MAX_ENCOUNTERS = 5;
 
     //each round, a Warrior and a Monster will be instantiated
-    private Warrior pat;   //Is it man or woman?
-    private Monster smaug; //Friendly generic monster name, eh?
+    private Character pat;   //Is it man or woman?
+    
+    private Character smaug; //Friendly generic monster name, eh?
 
     private int moveCount;
     private boolean gameOver;
@@ -49,6 +50,8 @@ public class YoRPG {
     public void newGame() {
 
 	String s;
+	String t;
+	int i;
 	String name = "";
 	s = "Welcome to Ye Olde RPG!\n";
 
@@ -72,9 +75,40 @@ public class YoRPG {
 	}
 	catch ( IOException e ) { }
 
+	t = "\nWhat class shall you choose?\n";
+	t += "\t1:Warrior\n";
+	t += "\t2:Elf\n";
+	t += "\t3:Dwarf\n";
+	t += "\t4:Wizard\n";
+	t += "\t5:Hobbit\n";
+	System.out.println(t);
+	
+	i = 1;
+	try {
+		i = Integer.parseInt( in.readLine() );
+	}
+	catch ( IOException e) { }
+	
+	if (i == 1) {
+		pat = new Warrior(name);
+		 }
+	else if (i == 2) {
+		pat = new Elf(name);
+		}
+	else if (i == 3) {
+		pat = new Dwarf(name);
+		}
+	else if (i == 4) {
+		pat = new Wizard(name);
+		}
+	else if (i == 5) {
+		pat = new Hobbit(name);
+		}
+	
+
 
 	//instantiate the player's character
-	pat = new Warrior( name );
+	;
 
     }//end newGame()
 
@@ -89,14 +123,21 @@ public class YoRPG {
 
 	int i = 1;
 	int d1, d2;
+	double diff = (difficulty / 3.0);
+	double MonsterRan = Math.random();
 
-	if ( Math.random() >= ( difficulty / 3.0 ) )
+	if ( Math.random() >=  diff  )
 	    System.out.println( "Nothing to see here. Move along!" );
 
 	else {
-	    System.out.println( "Lo, yonder monster approacheth!" );
+	    
+	    if (MonsterRan > (1.0 - diff)) {
+	    	smaug = new Urakhai();
+		System.out.println( "A powerful enemy emerges!" );}
 
-	    smaug = new Monster();
+	    else {
+		smaug = new Monster();
+		System.out.println( "Lo, yonder monster approacheth!" );}
 
 	    while( smaug.isAlive() && pat.isAlive() ) {
 
@@ -104,25 +145,34 @@ public class YoRPG {
 		// If you land a hit, you incur greater damage,
 		// ...but if you get hit, you take more damage.
 		try {
-		    System.out.println( "Do you feel lucky?" );
-		    System.out.println( "\t1: Nay.\n\t2: Aye!" );
+		    System.out.println( "What shall you do?" );
+		    System.out.println( "\t1: Normal Attack!\n\t2: Increase ATT but Lower Def!\n\t3:SPECIAL ATTACK!!!" );
 		    i = Integer.parseInt( in.readLine() );
 		}
 		catch ( IOException e ) { }
+	
+		if (i == 3)
+			d1 = pat.specialAtt (smaug);
 
-		if ( i == 2 )
-		    pat.specialize();
-		else
-		    pat.normalize();
+		else {
 
-		d1 = pat.attack( smaug );
+			if ( i == 2 )
+			    pat.specialize();
+			else if ( i == 1 )
+			    pat.normalize();
+
+			d1 = pat.attack( smaug ); 
+		}
+		
 		d2 = smaug.attack( pat );
 
-		System.out.println( pat.getName() + " dealt " + d1 +
-				    " points of damage.");
+		System.out.println( "You dealt  a total of " + d1 +
+				     " points of damage.");
 
 		System.out.println( "Ye Olde Monster hit back for " + d2 +
 				    " points of damage.");
+		System.out.println("You have " + pat.getHP() + "HP");
+	    	System.out.println("Ye Monster has " + smaug.getHP() + "HP");
 	    }//end while
 
 	    //option 1: you & the monster perish
